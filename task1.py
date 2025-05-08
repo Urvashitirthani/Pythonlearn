79ecdc07d5e00b3b620776d704802b3be5064d81
Urvashitirthani <tirthaniurvashi1623@gmail.com>
2025-05-08 22:47:20 +0530

    Update task1.py

---
+++ task1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:\t", a + b)
+print("Subtraction:\t", a - b)
+print("Multiplication:\t", a * b)
+print("Division:\t", a / b)
